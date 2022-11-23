6f4477f30eceace7a52dcf7e1057f11943965616
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-23 10:13:27 +0530

    Checking Daily Wage For Employee

--- EmployeeWageProgram.cs
+++ EmployeeWageProgram.cs
@@ -11,13 +11,23 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Conmputation");
+            const int EMP_RATE_PER_HR = 20;
             int employeePresent = 1;
+            int empWage = 0;
+            int empHrs = 0;
             Random r = new Random();
             int employeeInput = r.Next(0, 2);
             if (employeePresent == employeeInput)
+            {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
             else
+            {
                 Console.WriteLine("Employee is Absent");
+            }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Daily Employee Wage is:" + empWage);
             Console.ReadLine();
         }
     }
