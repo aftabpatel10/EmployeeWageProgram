d31b931acd21eae5e6a2cef243b470393813147e
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-24 08:23:51 +0530

    Checking Employee is Part Time or Not Using Switch Case

--- EmployeeWageProgram.cs
+++ EmployeeWageProgram.cs
@@ -14,24 +14,23 @@ namespace EmployeeWageProgram
             const int EMP_RATE_PER_HR = 20;
             int empWage = 0;
             int empHrs = 0;
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
             Random r = new Random();
             int employeeInput = r.Next(0, 3);
-            if (employeeInput == IS_PART_TIME)
+            switch (employeeInput)
             {
-                Console.WriteLine("Employee is Part time Present");
-                empHrs = 4;
-            }
-            else if (employeeInput == IS_FULL_TIME)
-            {
-                empHrs = 8;
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Daily Employee Wage is:" + empWage);
