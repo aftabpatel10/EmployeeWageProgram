88943cd07d6511ef01de120b2208a26222b94b19
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-24 08:34:04 +0530

    Cheking a Employee Wage Till Reached Totals Hrs or Days

--- EmployeeWageProgram.cs
+++ EmployeeWageProgram.cs
@@ -14,11 +14,13 @@ namespace EmployeeWageProgram
             const int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
             const int NO_OF_WORKING_DAYS = 2;
-            int empWage = 0;
+            const int MAX_HRS_OF_MONTH = 10;
+            int totalEmpHrs = 0;
             int empHrs = 0;
-            int totalEmpwage = 0;
-            for (int i = 0; i < NO_OF_WORKING_DAYS; i++)
+            int totalWorkingDays = 0;
+            while (totalWorkingDays <= NO_OF_WORKING_DAYS && totalEmpHrs <= MAX_HRS_OF_MONTH)
             {
+                totalWorkingDays++;
                 Random r = new Random();
                 int employeeInput = r.Next(0, 3);
                 switch (employeeInput)
@@ -35,12 +37,12 @@ namespace EmployeeWageProgram
                         empHrs = 0;
                         break;
                 }
-                empWage = EMP_RATE_PER_HR * empHrs;
-                totalEmpwage += empWage;
-                Console.WriteLine("Daily Employee Wage is:" + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs:" + totalEmpHrs);
                 Console.ReadLine();
             }
-            Console.WriteLine("Total emp Wage is:" + totalEmpwage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Total emp Wage is:" + totalEmpWage);
         }
     }
 }
